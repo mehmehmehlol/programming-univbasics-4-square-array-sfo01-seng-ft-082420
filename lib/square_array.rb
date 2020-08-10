6b48e5ca92a5609dbaaa75c77bc58bf7883847f3
Megan Lo <hkmeganlo@gmail.com>
2020-08-10 20:50:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   element = 0
   new_array = []
   while element < array.length do
-    new_element = array[element] ** 2
-    new_array.push(new_element)
+    //new_element = array[element] ** 2
+    new_array.push(array[element] ** 2)
     element += 1
   end
   new_array
