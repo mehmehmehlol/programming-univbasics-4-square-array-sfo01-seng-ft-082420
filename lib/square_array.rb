f96057bf3f44b62867fe8ad09e1d312f82ea3f8d
Megan Lo <hkmeganlo@gmail.com>
2020-08-10 20:40:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
+require 'pry'
 def square_array(array)
   # your code here
   element = 0
   new_array = []
   while element < array.length do
+    binding.pry
     new_element = array[element] ** 2
     new_array << new_element
     element += 1
